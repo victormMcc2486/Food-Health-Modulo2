9ef5bc041c7c848472c6f3416bc5329ea69debf3
Xhanty <38590378+Xhanty@users.noreply.github.com>
2019-08-14 13:46:53 -0500

    Insert Ok

---
+++ Santiago-Smith-Delgado-Henao/Food_Health_Modulo_2/Food_Health_Modulo_2/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Food_Health_Modulo_2.Startup))]
+namespace Food_Health_Modulo_2
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
