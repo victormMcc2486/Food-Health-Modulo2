4cab8b68f74bb1a5203330fe906a39977c68d3bb
victormMcc2486 <38736562+victormMcc2486@users.noreply.github.com>
2019-08-26 23:45:00 -0500

    insert ok

---
+++ Víctor-Manuel-Junior-Castillo-Cano/Modulo2/ListarClientes.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Modelo;
+
+namespace Modulo2
+{
+    public partial class ListarClientes : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Listar();
+        }
+
+        protected void Listar()
+        {
+            gvListadoClientes.DataSource = Usuarios.ListarClientes();
+
+            gvListadoClientes.DataBind();
+        }
+    }
+}
---
+++ Víctor-Manuel-Junior-Castillo-Cano/Modulo2/ListarUsuarios.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Modelo;
+
+namespace Modulo2
+{
+    public partial class ListarUsuarios1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Listar();
+        }
+        protected void Listar()
+        {
+            gvListadoUsuario.DataSource = Usuarios.ListarUsuarios();
+
+            gvListadoUsuario.DataBind();
+        }
+    }
+}
